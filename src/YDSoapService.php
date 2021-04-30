141eadc8030db8b89a698d014e2e4e84bee537bf
djcmendes-surface <djcmendes@gmail.com>
2021-04-30 16:52:47 +0100

    t

--- src/YDSoapService.php
+++ src/YDSoapService.php
@@ -1,18 +1,37 @@
 <?php
-namespace YDSoapService;
+namespace djcmendes\ydsoapservice;
 class YDSoapService
 {
-    public static function teste()
-    {
-        return 'Hello World com composer!';
+    //private $wsdl= null;
+    private $conn = null;
+    function __construct($url,$params) {
+        try{
+            $this->conn = new \SoapClient($url,$params); 
+        }catch(\SoapFault $e){
+            echo ('<br>failed request<br>');
+        }
+        catch(\Throwable $e){
+            echo ('<br>failed request<br>');
+        }
+    }
+
+    public function request($params,$func){
+        try{
+            if(is_object($params)){
+                $obj_name = getClass($params);
+                $params = new SoapParam(new SoapVar($params, SOAP_ENC_OBJECT),$obj_name);
+            }elseif(!is_array($params)){
+                throw new Exception('wrong input type');
+            }
+
+            $res = $this->conn->$func($params);
+            
+            return $xml = new \SimpleXMLElement($res);
+        }catch(\SoapFault $e){
+            echo ('<br>failed request<br>');
+        }
+        catch(\Throwable $e){
+            echo ('<br>failed request<br>');
+        }
     }
 }
-
-
-
-,
-    "autoload":{
-        "psr-4":{
-            "dmyd\\YDSoapService\\":"./vendor/YDSoapService"
-        }
-     }
